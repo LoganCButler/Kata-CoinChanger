6448df3963b9d9586111e389d5b64ef201530115
Logan Butler <logan.butler@agilethought.com>
2016-07-06 16:31:19 -0400

    Added a Try Catch to handle wrong inputs.

--- CoinChanger/CoinChanger/Program.cs
+++ CoinChanger/CoinChanger/Program.cs
@@ -10,15 +10,23 @@ namespace CoinChanger
     class Program
     {
         static void Main(string[] args)
-        {          
-            while (true)
+        {
+            try
             {
-                Application.CoinChanger c = new Application.CoinChanger();
-                Console.WriteLine("Please enter a monetary amount between $0.99 and $0.00");
-                var hold = Convert.ToDouble(Console.ReadLine());
-                c.ChangeCheck(hold);
-                Console.WriteLine("The change is... \n" + c.changeAmoutString);
-                Console.WriteLine("Would you like to do another?");
+                while (true)
+                {
+                    Application.CoinChanger c = new Application.CoinChanger();
+                    Console.WriteLine("Please enter a monetary amount between $0.99 and $0.00");
+                    var hold = Convert.ToDouble(Console.ReadLine());
+                    c.ChangeCheck(hold);
+                    Console.WriteLine("The change is... \n" + c.changeAmoutString);
+                    Console.WriteLine("Would you like to do another?");
+                }
+            }
+            catch (Exception)
+            {
+
+                return;
             }
 
         }

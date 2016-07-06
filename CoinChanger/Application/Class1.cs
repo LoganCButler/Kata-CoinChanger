907acce72cac5752cd29ee5c1769419984b6a6aa
Logan Butler <logan.butler@agilethought.com>
2016-07-06 09:52:27 -0400

    Pass OnePenny Test

--- CoinChanger/Application/Class1.cs
+++ CoinChanger/Application/Class1.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Application
 {
-    public class Class1
+    public class CoinChanger
     {
+        int penies;
+        int nickles;
+        int dimes;
+        int quarters;
+        string changeAmoutString;
+        int cents;
+
+        public string MakeChange(double input)
+        {
+            
+            cents = Convert.ToInt32(input*100); 
+            getPennies();
+            makeChangeString();
+            return changeAmoutString;
+        }
+
+        private void makeChangeString()
+        {
+            if (penies > 0)
+            {
+                changeAmoutString = "Pennies: " + penies.ToString();
+            }
+        }
+
+        private void getPennies()
+        {
+            for(var x = cents; x > 0; x--)
+            {
+                penies += 1;
+            }
+        }
     }
 }
--- CoinChanger/UnitTest/UnitTest1.cs
+++ CoinChanger/UnitTest/UnitTest1.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application;
 
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class SingleCoins
     {
         [TestMethod]
-        public void TestMethod1()
+        public void OnePenny()
         {
+            double input = 0.01;
+            CoinChanger c = new CoinChanger();
+
+            string output = c.MakeChange(input);
+
+            Assert.AreEqual("Pennies: 1", output);
+            
         }
     }
 }
